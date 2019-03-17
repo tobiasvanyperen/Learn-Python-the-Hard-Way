dfb3d8cdd106e39098139ee0c4b0403a050d8127
Tobias van Yperen <tobiasvanyperen@outlook.com>
2019-03-17 12:47:55 +0100

    Redo of ex13 and finish ex14

--- lpthw/ex13.py
+++ lpthw/ex13.py
@@ -1,14 +1,16 @@
 from sys import argv
+# read the WYSS section for how to run this
 input("Write down the prime word: ")
 input("Write down the first word: ")
 input("Write down the second word: ")
 input("Write down the third word: ")
 input("Write down the fourth word: ")
-# read the WYSS section for how to run this
+
 script, first, second, third, fourth = argv
 
-print("The script is called:", script)
+print("The script is called: ", script)
 print("Your first variable is:", first)
 print("Your second variable is:", second)
 print("Your third variable is:", third)
 print("Your fourth variable is:", fourth)
+
